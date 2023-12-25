24ca896a1ee42d23b37fa97aa6648f49dd127dfe
callumr00 <callum.rafter@protonmail.com>
2023-12-25 09:56:21 +0000

    :heavy_plus_sign: Added storage and installation of npm packages.

--- update_packages.sh
+++ update_packages.sh
@@ -5,3 +5,6 @@ pacman -Qqen > ~/.dotfiles/pacman_packages.txt
 
 # Save aur package list.
 pacman -Qqem > ~/.dotfiles/aur_packages.txt
+
+# Save npm package list.
+npm ls | grep '──' | awk '{print $2}' | awk -F@ '{print $1}' > ~/.dotfiles/npm_packages.txt
